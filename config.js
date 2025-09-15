7e56200bf14f0317e76057a27c82365245d0b162
jin1963 <kaojino89@gmail.com>
2025-09-15 19:22:14 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+
+// Address ที่ใช้จริง
+const contractAddress = "0xb3F2f75C5278E3a98c3E6b69F73768B7bd337421"; // Staking contract
+const tokenAddress = "0xd479ae350dc24168e8db863c5413c35fb2044ecd";   // KJC token
+const chainId = 56; // BSC Mainnet
+
+// export ถ้าใช้ ES6 module
+// export { contractAddress, tokenAddress, chainId };
